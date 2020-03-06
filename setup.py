7d02406f2bb693e1d27326a129e6d80b7eb8d624
AndreyVM <newbie@auriz.ru>
2020-03-06 10:43:53 +0500

    Create the package

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+from mp3pus import version
+
+
+setup(
+    name='mp3pus',
+    version=version,
+    packages=find_packages(),
+    python_requires='~=3.7',
+    install_requires=['mutagen>=1.40'],
+    zip_safe=False,
+    scripts=['bin/mp3pus'],
+    author='AndreyVM',
+    author_email='webmaster@auriz.ru',
+    description='A simple mp3 to opus converter.',
+    license='GNU GPLv3',
+    keywords='mp3 opus converter',
+    url='https://auriz.ru/d7Po8UxK')
